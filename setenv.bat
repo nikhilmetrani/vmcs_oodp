e9eef951c5f38cf067b5c78bbc064399dcd1075b
Janahan <sjanahan@gmail.com>
2015-10-25 12:10:31 +0800

    Command pattern Document added

--- setenv.bat
+++ setenv.bat
@@ -1,5 +1,6 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_20
-set ANT_HOME=C:\Bin\apache-ant-1.9.4
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_60
+set ANT_HOME=C:\Softwares\apache-ant-1.9.6-bin\apache-ant-1.9.6
+
 set JUNIT_HOME=.\jars
 
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
